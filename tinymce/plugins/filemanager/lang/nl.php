cd82e168715ce2930ef0305c591d38752d7bc607
Bmore <info@bmore.be>
2013-07-01 09:14:31 +0200

    Create nl.php
    Dutch language translation file

---
+++ tinymce/plugins/filemanager/lang/nl.php
@@ -0,0 +1,25 @@
+<?php
+define('lang_Select','Selecteer');
+define('lang_Erase','Wissen');
+define('lang_Open','Openen');
+define('lang_Confirm_del','Bent u zeker dat u dit bestand wilt wissen?');
+define('lang_All','Alles');
+define('lang_Files','Documenten');
+define('lang_Images','Foto\'s');
+define('lang_Archives','zip');
+define('lang_Error_Upload','Het geuploade bestand overschrijdt de maximum toegestane grootte.');
+define('lang_Error_extension','Dit bestandstype is niet toegestaan.');
+define('lang_Upload_file','Bestand Uploaden');
+define('lang_Filter','Filter');
+define('lang_Videos','Videos');
+define('lang_Music','Muziek');
+define('lang_New_Folder','Nieuwe Map');
+define('lang_Folder_Created','Map correct aangemaakt');
+define('lang_Existing_Folder','Bestaande map');
+define('lang_Confirm_Folder_del','Bent u zeker dat u deze map en alle onderdelen erin wilt wissen?');
+define('lang_Return_Files_List','Terug naar de bestandslijst');
+define('lang_Preview','Voorbeeld');
+define('lang_Download','Download');
+define('lang_Insert_Folder_Name','Kies nieuwe mapnaam:');
+define('lang_Root','root');
+?>
